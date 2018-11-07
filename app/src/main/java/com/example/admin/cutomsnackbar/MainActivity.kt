d35f7b8258d2a1a30ebed398622e474ad97457f2
admin <bob_the_besthotmail.com>
2018-11-07 23:42:30 +0100

    first commit

---
+++ app/src/main/java/com/example/admin/cutomsnackbar/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.admin.cutomsnackbar
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        welcomeButton.setOnClickListener {
+            Snackbar.make(mainLayout, "Welcome!! ${firstName.text}", Snackbar.LENGTH_INDEFINITE).show()
+        }
+    }
+}
